cbe9cc3bd9f9fa28759e8d818ed00409f33ead82
Vanessa <160286052+AmaCodez@users.noreply.github.com>
2025-04-18 17:29:36 -0400

    Create a function that fetches data from API Create a function that extracts needed data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import "./styles.css";
 
-console.log("js workss");
+const input = document.querySelector("input");
+const searchBtn = document.querySelector(".searchbtn");
+
+// Fetch + wait for data
+async function getWeather(location) {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=CJJE5U6F5XJJQRGBDQ7WCQQR3`;
+
+  const respond = await fetch(url, { mode: "cors" });
+  const unwrapData = await respond.json();
+  console.log(unwrapData);
+  //   console.log(`Conditions: ${unwrapData.days[0].conditions}`);
+  //   console.log(`Temp: ${unwrapData.days[0].temp}`);
+  //   console.log(`Tempmax: ${unwrapData.days[0].tempmax}`);
+  //   console.log(`Tempmin: ${unwrapData.days[0].tempmin}`);
+  return unwrapData;
+}
+
+//   Collect needed data
+function extractWeather(data) {
+  return {
+    conditions: data.currentConditions.conditions,
+    temp: data.currentConditions.temp,
+    tempmax: data.days[0].tempmax,
+    tempmin: data.days[0].tempmin,
+  };
+}
+
+searchBtn.addEventListener("click", async () => {
+  // For extractor to get the actual data and not a promise
+  try {
+    const searchValue = input.value;
+    const rawData = await getWeather(searchValue);
+    const summary = extractWeather(rawData);
+    console.log(`Weather summary:`, summary);
+  } catch (error) {
+    console.log(`Oh oh ${error.message}`);
+  }
+});
